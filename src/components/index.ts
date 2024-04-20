0fdb2616340466f246e8f1466b61f7ec7c49f65a
Adil Hussain <adil@Adils-MacBook-Pro.local>
2024-04-20 12:08:47 +0530

    initial commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,34 @@
-export { Header } from "./header";
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
+
+export {
+    UpcomingEvents,
+    DealsChart,
+
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    ProjectCardSkeleton,
+    LatestActivitiesSkeleton,
+
+    DashboardTotalCountCard,
+    LatestActivities
+};
+
+export * from '../components/tags/user-tag'
+export * from '../components/text'
+export * from '../pages/tasks/accordion'
+export * from '../pages/tasks/form/description'
+export * from '../pages/tasks/form/due-date'
+export * from '../pages/tasks/form/header'
+export * from '../pages/tasks/form/stage'
+export * from '../pages/tasks/form/title'
+export * from '../pages/tasks/form/users'
